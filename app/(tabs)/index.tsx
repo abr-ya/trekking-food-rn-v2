f859c4c29e87a03a2cca06d3055fd37f6437e265
abr-ya <gamer05@list.ru>
2026-03-07 02:21:59 +0300

    feat: Add image to assets, add build commands

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,9 @@
 import { Image } from "expo-image";
 import { Text, View } from "react-native";
 
+// Local image is bundled and always works in release (no network needed)
+const localImage = require("../../assets/images/chip-and-dale-faces.png");
+
 export default function Index() {
   return (
     <View
@@ -12,7 +15,7 @@ export default function Index() {
     >
       <Text>Hello, World!</Text>
       <Image
-        source={{ uri: "https://www.disneyclips.com/images3/images/chip-and-dale-faces.png" }}
+        source={localImage}
         style={{ width: 240, height: 240, marginTop: 16 }}
         contentFit="contain"
       />
